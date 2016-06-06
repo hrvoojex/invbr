053f41254f8319390c8f8eba6faeda5908eb7c3c
hrvooje <hrvoojex>
2016-06-07 00:54:21 +0200

    Created a script for adding 10 character inv.nr. in a second column

---
+++ invbr.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+input_file = raw_input("Input file name: ")
+try:
+    data = open(input_file, "r")
+except:
+    print("No such file %s" % input_file)
+    sys.exit("Quitting...")
+count = 1
+for line in data:
+    line = line.rstrip()
+    line = line.split(" ")
+    if len(line) > 1:
+        line[1] = line[1] + str(''.join(chr(ord('0')) for i in xrange(10 - len(line[1]) - len(str(count))))) + str(count)
+    else:
+        line.append((str(''.join(chr(ord('0')) for i in xrange(10 - len(str(count)))))) + str(count))
+    count += 1
+    print line
