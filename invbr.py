8f14d13f368fc1c6be091af454496c8de5077384
hrvoojex <hrvooje@gmail.com>
2016-06-07 09:05:37 +0200

    BRake lines not to be longer than 79

--- invbr.py
+++ invbr.py
@@ -14,8 +14,11 @@ for line in data:
     line = line.rstrip()
     line = line.split(" ")
     if len(line) > 1:
-        line[1] = line[1] + str(''.join(chr(ord('0')) for i in xrange(10 - len(line[1]) - len(str(count))))) + str(count)
+        line[1] = line[1] + str(''.join(chr(ord('0')) \
+                for i in xrange(10 - len(line[1]) - \
+                len(str(count))))) + str(count)
     else:
-        line.append((str(''.join(chr(ord('0')) for i in xrange(10 - len(str(count)))))) + str(count))
+        line.append((str(''.join(chr(ord('0')) \
+                for i in xrange(10 - len(str(count)))))) + str(count))
     count += 1
     print '{0:10s} {1:20s}'.format(line[0], line[1])
